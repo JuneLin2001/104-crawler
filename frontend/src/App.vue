05468d0f92e8ad127c88e1b70e3667c05d89dad1
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-20 02:56:04 +0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,13 +4,11 @@
       Job Scraper
     </h1>
 
-    <!-- 顯示已抓取頁數、總頁數、總工作數 -->
     <p class="text-xl text-gray-700 mb-4">
       已抓到 {{ pageCount }} 頁資料，共 {{ totalPages }} 頁
     </p>
-    <p class="text-xl text-gray-700 mb-4">共抓到 {{ totalItems }} 個工作</p>
+    <p class="text-xl text-gray-700 mb-4">共有 {{ totalItems }} 個工作</p>
 
-    <!-- 進度條 -->
     <div class="w-full bg-gray-200 rounded-full h-4 mb-4">
       <div
         class="bg-green-500 h-4 rounded-full"
@@ -21,7 +19,6 @@
       進度: {{ progress.toFixed(2) }}%
     </p>
 
-    <!-- 顯示工作列表 -->
     <h3 class="text-2xl text-gray-800 mt-6">爬取的工作列表：</h3>
     <ul class="space-y-4 mt-4">
       <li
@@ -33,11 +30,23 @@
           {{ job["Job Name"] }}
         </h4>
         <p class="text-gray-700 mt-2">
-          公司名稱: <span>{{ job["Company Name"] }}</span>
+          公司名稱: <span class="">{{ job["Company Name"] }}</span>
         </p>
         <p class="text-gray-700">
-          地址: <span>{{ job["Job Address"] }}</span>
+          地址: <span class="">{{ job["Job Address"] }}</span>
         </p>
+
+        <!-- 顯示標籤 -->
+        <div class="mt-2">
+          <span
+            v-for="label in job.Labels"
+            :key="label"
+            class="inline-block text-xs bg-blue-200 text-blue-700 rounded px-2 py-1 mr-2"
+          >
+            {{ label }}
+          </span>
+        </div>
+
         <a
           :href="job['Job Link']"
           target="_blank"
@@ -55,7 +64,7 @@
     data() {
       return {
         progress: 0, // 進度條百分比
-        pageCount: 0, // 已抓取的頁數
+        pageCount: 0, // 用來顯示抓取的頁數
         totalPages: 0, // 總頁數
         totalItems: 0, // 總項目數
         jobs: [], // 存儲爬取的工作資料
@@ -72,7 +81,6 @@
         if (data.page_count !== undefined && data.total_pages !== undefined) {
           this.pageCount = data.page_count;
           this.totalPages = data.total_pages;
-          this.totalItems = data.total_items; // 更新總項目數
           this.progress = (this.pageCount / this.totalPages) * 100; // 計算進度條百分比
         }
 
@@ -96,7 +104,3 @@
     },
   };
 </script>
-
-<style scoped>
-  /* 這裡使用 Tailwind 的樣式，不需要再額外添加樣式 */
-</style>
